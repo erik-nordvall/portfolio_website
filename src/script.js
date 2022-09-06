a41de98bb1dfad10e925804b0292c7b86c4e9ee2
Erik Nordvall <erino05@ad.liu.se>
2022-09-06 16:01:29 +0200

    Textures for galaxy added

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
 import gsap from 'gsap'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
 
 /**
  * Debug
@@ -33,46 +35,104 @@ const scene = new THREE.Scene()
  * Objects
  */
 //Materials
+const lettermaterial = new THREE.MeshNormalMaterial()
+
+const glassmaterial  = new THREE.MeshPhysicalMaterial({
+    //metalness: 0,  
+    roughness: 0.2,
+    transmission: 1,
+    thickness: 0.5, // Add refraction!
+  });
+
 const material = new THREE.MeshStandardMaterial({color: parameters.materialColor})
 
-const objectsDistance = 4
 
+//Meshes
+
+//Mesh 1
+const Mesh1 = new THREE.Group()
+const objectsDistance = 4
 const mesh1 = new THREE.Mesh(
-    new THREE.TorusGeometry(1, 0.4, 16, 60),
-    material, 
+    new THREE.TorusGeometry(3, 0.4, 16, 60),
+    glassmaterial, 
 )
-mesh1.position.x = 2
+mesh1.position.x = 0
+mesh1.position.z = 1
 mesh1.position.y = - objectsDistance * 0
 
+Mesh1.add(mesh1)
 
-const mesh2 = new THREE.Mesh(
-    new THREE.ConeGeometry(1, 2, 64),
-    material
-)
-mesh2.position.x = - 2
-mesh2.position.y = - objectsDistance * 1
+//const mesh2 = new THREE.Mesh(
+//    new THREE.ConeGeometry(1, 2, 64),
+//    material
+//)
+//mesh2.position.x = - 2
+//mesh2.position.y = - objectsDistance * 1
 
-const mesh3 = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 32),
-    material
+//const mesh3 = new THREE.Mesh(
+//    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 32),
+//    material
+//)
+//mesh3.position.x = 2
+//mesh3.position.y = - objectsDistance * 2
+
+/**
+ * Models
+ */
+let mixer = null
+ const gltfLoader = new GLTFLoader()
+ gltfLoader.load(
+    '/models/name_reveal.glb',
+    (gltf) =>
+    {
+        
+        mixer = new THREE.AnimationMixer(gltf.scene)
+        for(let i = 0; i<gltf.animations.length; i++){
+            const animation = mixer.clipAction(gltf.animations[i])
+            animation.setLoop(THREE.LoopOnce);
+            animation.clampWhenFinished = true;
+            animation.enable = true;
+            animation.play()
+        }
+        
+        while(gltf.scene.children.length)
+        {
+            console.log(gltf.scene.children[0].children[0].material.color.r)
+            gltf.scene.children[0].children[0].material = lettermaterial
+            const letters = new THREE.Group()
+            letters.add(gltf.scene.children[0])
+            letters.scale.set(0.8, 0.8, 0.8)
+            letters.position.set(-2, -0.5, 2)
+            scene.add(letters)
+            //scene.add(gltf.scene.children[0])
+        }
+        //console.log(scene)
+    }
 )
-mesh3.position.x = 2
-mesh3.position.y = - objectsDistance * 2
+
 
 /**
  * Lights
  */
- const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
- directionalLight.position.set(1, 1, 0)
+ const directionalLight1 = new THREE.DirectionalLight('#ffffff', 2)
+ directionalLight1.position.set(1, 1, 0)
+
+const directionalLight2 = new THREE.DirectionalLight('#ffffff', 2)
+directionalLight2.position.set(-2, -1, 0)
 
 
-scene.add(mesh1, mesh2, mesh3, directionalLight)
 
-const sectionMeshes = [mesh1, mesh2, mesh3]
+scene.add(directionalLight1, directionalLight2)
+
+const sectionMeshes = [Mesh1] //mesh2, mesh3]
 
 /**
  * Particles
  */
+
+//parallax particles
+const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 //Geometry
 const particlesCount = 500
 const positions = new Float32Array(particlesCount*3)
@@ -87,15 +147,47 @@ particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 
 
 //Material
 const particlesMaterial = new THREE.PointsMaterial({
-    color: parameters.materialColor,
+    color: 0xffffff,
     sizeAttenuation: true,
-    size: 0.03
+    size: 0.1,
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    map: particleTexture,
+    alphaMap: particleTexture,
+    depthWrite: false // instead of sortParticles
 
 })
 
 //Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
+
+
+//Donut particles
+
+function createPoints(geom) {
+    var material = new THREE.PointsMaterial({
+      color: 0xffffff,
+      size: 0.1,
+      transparent: true,
+      blending: THREE.AdditiveBlending,
+      map: particleTexture,
+      alphaMap: particleTexture,
+      depthWrite: false // instead of sortParticles
+    });
+  
+    let cloud = new THREE.Points(geom, material);
+    cloud.position.z = 1
+    return cloud;
+  }
+
+  //const donutgeom = new THREE.TorusGeometry(3, 0.4, 16, 60)
+  const donutgeom = new THREE.SphereGeometry(4, 8, 8)
+  const donut = createPoints(donutgeom)
+  
+  Mesh1.add(donut)
+  scene.add(Mesh1)
+
 
 /**
  * Sizes
@@ -194,6 +286,13 @@ const tick = () =>
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
 
+    //Update Mixer
+    if(mixer !== null)
+    {
+        mixer.update(deltaTime)
+
+    }
+
     //Animate Camera
     camera.position.y =  - scrollY / sizes.height * objectsDistance
 
@@ -206,7 +305,7 @@ const tick = () =>
     for(const mesh of sectionMeshes)
     {
         mesh.rotation.x += deltaTime * 0.1
-        mesh.rotation.y += deltaTime * 0.15 
+        mesh.rotation.y += deltaTime * 0.05
 
     }
 
